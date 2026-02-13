5d4fd747a8bc17f360601957894350e081ddb67e
Jonas Plank <Jonas.plank@tum.de>
2026-02-13 09:41:04 +0100

    Remove build and old folder

--- out/build/x64-Debug/UnitTests/LinearSolvers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jonas/source/repos/COME/UnitTests/LinearSolvers
-# Build directory: C:/Users/jonas/source/repos/COME/out/build/x64-Debug/UnitTests/LinearSolvers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
