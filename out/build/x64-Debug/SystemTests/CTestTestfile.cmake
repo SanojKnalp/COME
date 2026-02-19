21c94c7b294ce05b507843e8d3e2fcba00d7fb0f
Jonas Plank <Jonas.plank@tum.de>
2026-02-19 12:11:56 +0100

    Remove build folder

--- out/build/x64-Debug/SystemTests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jonas/private_stuff/FEL2/SystemTests
-# Build directory: C:/Users/jonas/private_stuff/FEL2/out/build/x64-Debug/SystemTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
